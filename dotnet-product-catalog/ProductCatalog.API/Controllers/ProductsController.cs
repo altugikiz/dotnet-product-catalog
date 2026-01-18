68d12da5e01ba1d404715d11ef848aa7acb8c4d9
Altuğ İkiz <altugikiz10@gmail.com>
2026-01-18 16:12:47 +0300

    Refactor ProductsController to return ApiResponse for GetAll and Create methods

--- dotnet-product-catalog/ProductCatalog.API/Controllers/ProductsController.cs
+++ dotnet-product-catalog/ProductCatalog.API/Controllers/ProductsController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using ProductCatalog.API.Dtos;
 using ProductCatalog.API.Services;
+using ProductCatalog.API.Wrappers;
 
 namespace ProductCatalog.API.Controllers;
 
@@ -10,18 +11,23 @@ public class ProductsController(IProductService productService) : ControllerBase
 {
     // 1. List all of products ( GET: api/products )
     [HttpGet]
-    public async Task<ActionResult<List<ProductDto>>> GetAll()
+    public async Task<ActionResult<ApiResponse<List<ProductDto>>>> GetAll()
     {
         // LINQ for JOIN Operations
         var products = await productService.GetAllAsync();
+        var response = ApiResponse<List<ProductDto>>.SuccessResult(products);
         return Ok(products); // HTTP 200
     }
 
     // 2. Add to new products ( POST: api/products )
     [HttpPost]
-    public async Task<ActionResult<ProductDto>> Create(CreateProductDto request)
+    public async Task<ActionResult<ApiResponse<ProductDto>>> Create(CreateProductDto request)
     {
-            var result = await productService.CreateAsync(request);
-            return StatusCode(201, result);
+            var product = await productService.CreateAsync(request);
+
+        // Created (201) 
+        var response = ApiResponse<ProductDto>.SuccessResult(product, 201, "The product has been created successfully.");
+
+        return StatusCode(201, response);
     }
 }
