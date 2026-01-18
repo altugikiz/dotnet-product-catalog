36648d605f8848916c2094a177b5db0be615fb46
Altuğ İkiz <altugikiz10@gmail.com>
2026-01-18 14:05:12 +0300

    Refactor ProductsController to use IProductService for product management

--- dotnet-product-catalog/ProductCatalog.API/Controllers/ProductsController.cs
+++ dotnet-product-catalog/ProductCatalog.API/Controllers/ProductsController.cs
@@ -1,30 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using ProductCatalog.API.Data;
 using ProductCatalog.API.Dtos;
-using ProductCatalog.API.Models;
+using ProductCatalog.API.Services;
 
 namespace ProductCatalog.API.Controllers;
 
 [Route("api/[controller]")] // URL : api/products
 [ApiController]
-public class ProductsController(AppDbContext context) : ControllerBase
+public class ProductsController(IProductService productService) : ControllerBase
 {
     // 1. List all of products ( GET: api/products )
     [HttpGet]
     public async Task<ActionResult<List<ProductDto>>> GetAll()
     {
         // LINQ for JOIN Operations
-        var products = await context.Products
-            .Include(p => p.Category) // Eager Loading ( SQL JOIN )
-            .Select(p => new ProductDto(  // Entity -> DTO
-                p.Id,
-                p.Name,
-                p.Price,
-                p.Category != null ? p.Category.Name : "Uncategorized" // Null Check
-            ))
-            .ToListAsync();
-
+        var products = await productService.GetAllAsync();
         return Ok(products); // HTTP 200
     }
 
@@ -32,34 +22,14 @@ public class ProductsController(AppDbContext context) : ControllerBase
     [HttpPost]
     public async Task<ActionResult<ProductDto>> Create(CreateProductDto request)
     {
-        // Is there any category?
-        var category = await context.Categories.FindAsync(request.CategoryId);
-        if(category == null)
+        try
         {
-            return BadRequest("Invalid Category ID!"); // HTTP 400
+            var result = await productService.CreateAsync(request);
+            return StatusCode(201, result);
         }
-
-        // DTO -> Entity
-        var product = new Product
+        catch (Exception ex)
         {
-            Name = request.Name,
-            Price = request.Price,
-            CategoryId = request.CategoryId
-        };
-
-        // To add to database
-        context.Products.Add(product);
-        await context.SaveChangesAsync(); 
-
-        // Return created data
-        var responseDto = new ProductDto(
-            product.Id,
-            product.Name,
-            product.Price,
-            product.Name
-        );
-
-        // HTTP 201 created and add location information to Header
-        return CreatedAtAction(nameof(GetAll), new { id = product.Id }, responseDto);
+            return BadRequest(ex.Message);
+        }
     }
 }
