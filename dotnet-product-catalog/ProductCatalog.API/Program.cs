69ac76f04e263a8d2c2e3f2a842a55ddaebe8b54
Altuğ İkiz <altugikiz10@gmail.com>
2026-01-18 10:02:19 +0300

    Ensure temporary database creation on application startup

--- dotnet-product-catalog/ProductCatalog.API/Program.cs
+++ dotnet-product-catalog/ProductCatalog.API/Program.cs
@@ -21,6 +21,13 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 
 var app = builder.Build();
 
+// The application creates a temporary database and prints the Seed Data when it starts up.
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated(); // Trigger
+}
+
 // 2. --- Middleware Pipeline ---
 
 // To open Swagger UI
